34035ac93a63958e401f42f5a30640fbe7d3338a
Priyanka Gupta <pynk1992@gmail.com>
2023-10-22 23:14:15 +0200

    css done for menu page.

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -3,20 +3,34 @@ import styles from '@/styles/Menu.module.css'
 
 export default function Menu() {
   return(
-    <>
+  <div className={styles['menu-page']}>
      <h3 className={styles['menu-item--text']}>On menu today...</h3>
-     <div className={styles['menu-item']}>
-    <div className={styles['menu-item--image']}>
-    <Image src='/chole 1.png' height='150' width='150' alt='chole'/>
-    < div className={styles['menu-item__name--price']}>
-      <h3>Chola Bhatura</h3>
-      <h4>€ 9.5</h4>
-    </div>
-      <div className={styles['menu-item--button']}>Add</div>
-    </div>
-
-    </div>
-    </>
-   
+     <div className={styles['menu-items']}> 
+        <div className={styles['menu-item']}>
+          <div className={styles['menu-item--img']}>
+             <Image src='/chole 1.png' height='150' width='150' alt='chole'/>
+           </div>     
+          <div className={styles['menu-item__details']}>
+              <h3 className={styles['menu-item__details--name']}>Chola Bhatura</h3>
+              <h4 className={styles['menu-item__details--price']}>€ 9.5</h4>
+          </div>
+           <div className={styles['menu-item--button']}>
+            <button className={styles['btn-wrapper']}>Add</button>
+            </div>               
+        </div><div className={styles['menu-item']}>
+          <div className={styles['menu-item--img']}>
+             <Image src='/chole 1.png' height='150' width='150' alt='chole'/>
+           </div>     
+          <div className={styles['menu-item__details']}>
+              <h3 className={styles['menu-item__details--name']}>Chola Bhatura</h3>
+              <h4 className={styles['menu-item__details--price']}>€ 9.5</h4>
+          </div>
+           <div className={styles['menu-item--button']}>
+            <button className={styles['btn-wrapper']}>Add</button>
+            </div>               
+        </div>
+     </div>  
+  </div>
+    
   )
 }       
