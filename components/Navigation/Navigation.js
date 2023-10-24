cb3c8bece19da12bef98474b0646307a39d6ab2c
Priyanka Gupta <pynk1992@gmail.com>
2023-10-24 02:14:18 +0200

    Added functionality to add to cart button.

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from './Navigation.module.css'
+import { IconShoppingCart } from "@tabler/icons-react";
 
 export default function Navigation(){
   return(
@@ -9,6 +10,11 @@ export default function Navigation(){
     </div>
     <div className={styles['nav-bar__link']}> <Link href={"./"}>Home</Link>
     <Link href={"./menu"}>Menu</Link>
+    <Link href="/checkout"><p>
+        <IconShoppingCart/>
+        <span>0</span>
+      </p>
+    </Link>
     <Link href={"./login"}>Login/Signup</Link>
     </div>
     </div>
--- pages/menu/index.js
+++ pages/menu/index.js
@@ -1,10 +1,45 @@
-import MenuItem from '@/components/MenuItem/MenuItem'
 import styles from '@/styles/Menu.module.css'
  import Image from 'next/image'
+ import useSWR from "swr"
 
+ const fetcher = (...args) => fetch(...args).then(res => res.json())
 export default function Menu() {
-  return(
-   <MenuItem/>
+    const { data, error, isLoading } = useSWR('/api/menus', fetcher)
+ 
+ 
+    if (error) return <div>failed to load</div>
+    if (isLoading) return <div>loading...</div>
 
-  )
+  const cart = []
+  function handelAddToCart(menu){
+  cart.push(menu)
+  console.log(cart)
+ }
+
+     return (
+      <div className={styles['menu-page']}>
+         <h3 className={styles['menu-item--text']}>On menu today...</h3>
+         <div className={styles['menu-items']}> 
+         {data.map((menu) =>
+         {return (
+          <div className={styles['menu-item']}>
+            <div className={styles['menu-item--img']}>
+            <Image src={menu.image} height='150' width='150' alt={menu.name}/>
+         </div>     
+         <div className={styles['menu-item__details']}>
+             <p className={styles['menu-item__details--name']}>{menu.name}</p>
+             <p className={styles['menu-item__details--price']}>€{menu.price}</p>
+         </div>
+         <div className={styles['menu-item--button']}>
+           <button onClick={() => handelAddToCart(menu)} className={styles['btn-wrapper']}>Add</button>
+         </div>               
+       </div>  
+         )
+         
+  })} 
+       </div>
+  
+    </div>
+      
+     )
 }       
